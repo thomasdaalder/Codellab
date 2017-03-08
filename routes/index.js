209a710487ae3c70bd91c71474a793ac3a5c7688
Thien <thientje@gmail.com>
2017-03-08 15:26:26 +0100

    added if session to navbar

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Code Matcher' });
+  const userSession = req.session.user
+  res.render('index', { title: 'Code Matcher', user: userSession });
 });
 
 module.exports = router;
